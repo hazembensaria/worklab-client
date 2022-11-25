0b37917629f055e06d37e4adbdde10739751fe37
amine_tt <mohamedaminetouihri9@gmail.com>
2022-11-25 15:25:02 +0100

    config routes and add tailwind

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LandingPageComponent } from './compenents/public /landing-page/landing-page.component';
+import { LoginComponent } from './compenents/public /login/login.component';
+import { RegisterComponent } from './compenents/public /register/register.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:"login",component:LoginComponent
+  },{
+    path:"register",component:RegisterComponent
+  },
+  {
+    path:"**",component:LandingPageComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LandingPageComponent } from './compenents/public /landing-page/landing-page.component';
+import { LoginComponent } from './compenents/public /login/login.component';
+import { RegisterComponent } from './compenents/public /register/register.component';
+import { NavComponent } from './compenents/shared/nav/nav.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingPageComponent,
+    LoginComponent,
+    RegisterComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule,
