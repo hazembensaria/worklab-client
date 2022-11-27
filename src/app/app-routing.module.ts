055b7110baa3f4582feb5c57843a95c3e0885eb8
amine_tt <mohamedaminetouihri9@gmail.com>
2022-11-27 20:19:00 +0100

    form0

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { LandingPageComponent } from './compenents/public /landing-page/landing-page.component';
-import { LoginComponent } from './compenents/public /login/login.component';
-import { RegisterComponent } from './compenents/public /register/register.component';
+import { LandingPageComponent } from './compenents/public/landing-page/landing-page.component';
+import { LoginComponent } from './compenents/public/login/login.component';
+import { RegisterComponent } from './compenents/public/register/register.component';
+
 
 const routes: Routes = [
   {
     path:"login",component:LoginComponent
-  },{
+  },
+  {
     path:"register",component:RegisterComponent
+
   },
   {
     path:"**",component:LandingPageComponent
